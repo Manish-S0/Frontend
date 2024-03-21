7ac25dde4c50b094caede36a8841cae4b95031f6
manish <manishkrsah88@gmail.com>
2024-03-21 16:31:52 +0545

    2nd project

---
+++ 02_new html/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import New from './new'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hello</h1>
+      <New />
+    </> // only one element can be returned so we write everything in <>'fragment element'
+  )
+}
+
+export default App
---
+++ 02_new html/vite-project/src/new.jsx
@@ -0,0 +1,10 @@
+function New(){
+  return(
+    <>
+    <h3>this is new project</h3>
+    </>
+  )
+  
+}
+
+export default New
